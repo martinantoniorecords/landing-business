1e0c3a698979091317c6675b1b9c08cf64fcc05d
Martin Antonio Records <martinantoniorecords@gmail.com>
2025-08-31 14:32:16 +0300

    Update project

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ChatBox from './ChatBox';
 function App() {
   return (
     <div>
-      <h1>Изработка на уебсайтове</h1>
+      <h1 style={{ textAlign: 'center' }}>Изработка на уебсайтове</h1>
       <BusinessLanding />
       <ChatBox />
     </div>
