7e334d83734e534535df95761dc678ff7f3debb6
Martin Antonio Records <martinantoniorecords@gmail.com>
2025-10-16 15:45:31 +0300

    Initial commit

--- src/BusinessLanding.js
+++ src/BusinessLanding.js
@@ -24,7 +24,7 @@ const BusinessLanding = () => {
         headers: {
           "Content-Type": "application/json",
           apikey:
-            "YOUR_SUPABASE_KEY",
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNuZXluYXN1emRja2NuaW1laGRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NjIxMzksImV4cCI6MjA2MTIzODEzOX0.4JZ_zHLCV2fJs872P5PB-9h0ZGFgLsXdksIajkrPrKU",
           Authorization:
             "Bearer YOUR_SUPABASE_KEY",
           Prefer: "return=minimal",
